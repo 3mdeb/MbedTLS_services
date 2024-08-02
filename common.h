ab3b6808a3992331552ceabba4b2591543447c2a
Tymoteusz Burak <tymoteusz.burak@3mdeb.com>
2024-08-02 17:01:17 +0200

    *: improve error handling

--- common.h
+++ common.h
@@ -16,7 +16,7 @@
 
 void initialize_mbedtls(mbedtls_ssl_context &ssl, mbedtls_ssl_config &ssl_conf, mbedtls_x509_crt &cacert, mbedtls_pk_context &key, mbedtls_x509_crt &cert, mbedtls_ctr_drbg_context &ctr_drbg, mbedtls_entropy_context &entropy);
 void cleanup_mbedtls(mbedtls_ssl_context &ssl, mbedtls_ssl_config &ssl_conf, mbedtls_x509_crt &cacert, mbedtls_pk_context &key, mbedtls_x509_crt &cert, mbedtls_ctr_drbg_context &ctr_drbg, mbedtls_entropy_context &entropy);
-void handle_error(int ret, const std::string &msg, int expected = 0);
+void handle_error(int ret, const std::string &msg, int expected = 0, bool print_ret = true);
 std::string get_ssl_verify_result(mbedtls_ssl_context &ssl);
 void debug_callback(void *ctx, int level, const char *file, int line, const char *str);
 
